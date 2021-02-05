2cfb2ad166404ecb682dbd1be8592fdd81ea99bb
nanmu <2573852113@qq.com>
2021-02-05 11:29:26 +0800

    更新答案

--- Week_01/dm_合并两个有序数组.c
+++ Week_01/dm_合并两个有序数组.c
@@ -1,24 +1,19 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     struct ListNode *next;
- * };
- */
-
-struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2) {
-	if (l1 == NULL) {
-		return l2;
+void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
+	int i, j;
+	int tmp;
+	for (i = m, j = 0; (i < nums1Size) && (j < n); i++, j++)
+	{
+		nums1[i] = nums2[j];
 	}
-	if (l2 == NULL) {
-		return l1;
-	}
-	if (l1->val <= l2->val) {
-		l1->next = mergeTwoLists(l1->next, l2);
-		return l1;
-	}
-	else {
-		l2->next = mergeTwoLists(l2->next, l1);
-		return l2;
+	for (i = 0; i < nums1Size; i++)
+	{
+		for (j = i + 1; j < nums1Size; j++)
+		{
+			if (nums1[i] > nums1[j]) {
+				tmp = nums1[i];
+				nums1[i] = nums1[j];
+				nums1[j] = tmp;
+			}
+		}
 	}
 }
